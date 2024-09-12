6014d8bad08ff9cf9c49caccc6a5d061431a7100
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-12 13:33:10 -0300

    Criei trigger referente a excluir marca sem produto

---
+++ sql/triggers/excluir_marca.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION deletarMarca()
+RETURNS TRIGGER AS $ $ BEGIN
+IF NOT EXISTS (
+    SELECT 1 FROM
+        Produto
+    WHERE
+        id_marca = OLD.id_marca
+) THEN
+DELETE FROM
+    Marca
+WHERE
+    id = OLD.id_marca;
+
+END IF;
+
+RETURN OLD;
+
+END;
+
+$ $ LANGUAGE plpgsql;
+
+CREATE TRIGGER deletarMarca
+AFTER
+    DELETE ON Produto FOR EACH ROW
+    WHEN (OLD.id_marca IS NOT NULL) EXECUTE FUNCTION deletarMarca();
