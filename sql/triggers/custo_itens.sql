fc305a2262a5cc1f8b8cf593ab59c8463f6db1ef
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-12 22:03:42 -0300

    Inseri os triggers de atualizar o custo_itens das vendas

--- sql/triggers/custo_itens.sql
+++ sql/triggers/custo_itens.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION somaCusto()
+CREATE FUNCTION somaCustoCadastro()
 RETURNS TRIGGER AS $$ BEGIN
 UPDATE
     Cadastro
@@ -13,7 +13,22 @@ END;
 
 $$ LANGUAGE plpgsql;;
 
-CREATE OR REPLACE FUNCTION subtraiCusto()
+CREATE FUNCTION somaCustoVenda()
+RETURNS TRIGGER AS $$ BEGIN
+UPDATE
+    Venda
+SET
+    custo_itens = custo_itens + NEW.preco
+WHERE
+    id = NEW.id_cadastro;
+
+RETURN NEW;
+
+END;
+
+$$ LANGUAGE plpgsql;;
+
+CREATE FUNCTION subtraiCustoCadastro()
 RETURNS TRIGGER AS $$ BEGIN
 UPDATE
     Cadastro
@@ -27,7 +42,21 @@ END;
 
 $$ LANGUAGE plpgsql;;
 
-CREATE OR REPLACE FUNCTION atualizarCusto()
+CREATE FUNCTION subtraiCustoVenda()
+RETURNS TRIGGER AS $$ BEGIN
+UPDATE
+    Venda
+SET
+    custo_itens = custo_itens - OLD.preco
+WHERE
+    id = OLD.id_cadastro;
+RETURN OLD;
+
+END;
+
+$$ LANGUAGE plpgsql;;
+
+CREATE FUNCTION atualizarCustoCadastro()
 RETURNS TRIGGER AS $$ BEGIN
 UPDATE
     Cadastro
@@ -41,17 +70,53 @@ END;
 
 $$ LANGUAGE plpgsql;;
 
+CREATE FUNCTION atualizarCustoVenda()
+RETURNS TRIGGER AS $$ BEGIN
+UPDATE
+    Venda
+SET
+    custo_itens = custo_itens - OLD.preco + NEW.preco
+WHERE
+    id = NEW.id_cadastro;
+RETURN NEW;
+
+END;
+
+$$ LANGUAGE plpgsql;;
+
 CREATE TRIGGER incrementarTotalCustoCadastro
-AFTER
-    INSERT ON Item FOR EACH ROW
-    WHEN (NEW.id_cadastro IS NOT NULL) EXECUTE FUNCTION somaCusto();
+    AFTER INSERT
+    ON Item FOR EACH ROW
+    WHEN (NEW.id_cadastro IS NOT NULL)
+    EXECUTE FUNCTION somaCustoCadastro();
+
+CREATE TRIGGER incrementarTotalCustoVenda
+    AFTER INSERT
+    ON Item FOR EACH ROW
+    WHEN (NEW.id_venda IS NOT NULL)
+    EXECUTE FUNCTION somaCustoVenda();
 
 CREATE TRIGGER decrementarTotalCustoCadastro
-AFTER
-    DELETE ON Item FOR EACH ROW
-    WHEN (OLD.id_cadastro IS NOT NULL) EXECUTE FUNCTION subtraiCusto();
+    AFTER DELETE
+    ON Item FOR EACH ROW
+    WHEN (OLD.id_cadastro IS NOT NULL)
+    EXECUTE FUNCTION subtraiCustoCadastro();
+
+CREATE TRIGGER decrementarTotalCustoVenda
+    AFTER DELETE
+    ON Item FOR EACH ROW
+    WHEN (OLD.id_venda IS NOT NULL)
+    EXECUTE FUNCTION subtraiCustoVenda();
 
 CREATE TRIGGER atualizarTotalCustoCadastro
-AFTER
-    UPDATE OF preco ON Item FOR EACH ROW
-    WHEN (NEW.id_cadastro IS NOT NULL) EXECUTE FUNCTION atualizarCusto();
+    AFTER UPDATE
+    OF preco ON Item
+    FOR EACH ROW
+    WHEN (NEW.id_cadastro IS NOT NULL)
+    EXECUTE FUNCTION atualizarCustoCadastro();
+
+CREATE TRIGGER atualizarTotalCustoVenda
+    AFTER UPDATE
+    OF preco ON Item FOR EACH ROW
+    WHEN (NEW.id_venda IS NOT NULL)
+    EXECUTE FUNCTION atualizarCustoVenda();
