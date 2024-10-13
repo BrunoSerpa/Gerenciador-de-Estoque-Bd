35513856edca7c6f04058997aa83da1abe215c2d
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-12 22:41:37 -0300

    Adicionei a relevância do id_venda

--- sql/triggers/quantidade.sql
+++ sql/triggers/quantidade.sql
@@ -31,9 +31,25 @@ $$ LANGUAGE plpgsql;;
 CREATE TRIGGER incrementarQuantidade
 AFTER
     INSERT ON Item FOR EACH ROW
-    WHEN (NEW.id_produto IS NOT NULL) EXECUTE FUNCTION adicionarItem();
+    WHEN (NEW.id_produto IS NOT NULL)
+    WHEN (NEW.id_venda IS NULL)
+    EXECUTE FUNCTION adicionarItem();
 
 CREATE TRIGGER decrementarQuantidade
 AFTER
     DELETE ON Item FOR EACH ROW
-    WHEN (OLD.id_produto IS NOT NULL) EXECUTE FUNCTION removerItem();
+    WHEN (OLD.id_produto IS NOT NULL)
+    WHEN (NEW.id_venda IS NULL)
+    EXECUTE FUNCTION removerItem();
+
+CREATE TRIGGER atualizarQuantidadeParaAdicionar
+AFTER UPDATE OF id_venda ON Item
+FOR EACH ROW
+WHEN (NEW.id_venda IS NULL AND OLD.id_venda IS NOT NULL)
+EXECUTE FUNCTION adicionarItem();
+
+CREATE TRIGGER atualizarQuantidadeParaRemover
+AFTER UPDATE OF id_venda ON Item
+FOR EACH ROW
+WHEN (NEW.id_venda IS NOT NULL AND OLD.id_venda IS NULL)
+EXECUTE FUNCTION removerItem();
