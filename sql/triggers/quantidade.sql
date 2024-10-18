a27f97b40bd61e0a14fdab1ca5fb50e904525316
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-18 11:44:51 -0300

    Formatei as quantidades

--- sql/triggers/quantidade.sql
+++ sql/triggers/quantidade.sql
@@ -1,55 +1,41 @@
-CREATE OR REPLACE FUNCTION adicionarItem()
+CREATE FUNCTION adicionarItem()
 RETURNS TRIGGER AS $$ BEGIN
-UPDATE
-    Produto
-SET
-    quantidade = quantidade + 1
-WHERE
-    id = NEW.id_produto;
-
-RETURN NEW;
-
+    UPDATE Produto
+        SET quantidade = quantidade + 1
+        WHERE id = NEW.id_produto;
+    RETURN NEW;
 END;
-
 $$ LANGUAGE plpgsql;;
 
-CREATE OR REPLACE FUNCTION removerItem()
+CREATE FUNCTION removerItem()
 RETURNS TRIGGER AS $$ BEGIN
-UPDATE
-    Produto
-SET
-    quantidade = quantidade - 1
-WHERE
-    id = OLD.id_produto;
-
-RETURN OLD;
-
+    UPDATE Produto
+        SET quantidade = quantidade - 1
+        WHERE id = OLD.id_produto;
+    RETURN OLD;
 END;
-
 $$ LANGUAGE plpgsql;;
 
 CREATE TRIGGER incrementarQuantidade
-AFTER
-    INSERT ON Item FOR EACH ROW
+    AFTER INSERT
+    ON Item FOR EACH ROW
     WHEN (NEW.id_produto IS NOT NULL)
-    WHEN (NEW.id_venda IS NULL)
     EXECUTE FUNCTION adicionarItem();
 
 CREATE TRIGGER decrementarQuantidade
-AFTER
-    DELETE ON Item FOR EACH ROW
-    WHEN (OLD.id_produto IS NOT NULL)
-    WHEN (NEW.id_venda IS NULL)
+    AFTER DELETE
+    ON Item FOR EACH ROW
+    WHEN (OLD.id_produto IS NOT NULL AND OLD.id_venda IS NULL)
     EXECUTE FUNCTION removerItem();
 
 CREATE TRIGGER atualizarQuantidadeParaAdicionar
-AFTER UPDATE OF id_venda ON Item
-FOR EACH ROW
-WHEN (NEW.id_venda IS NULL AND OLD.id_venda IS NOT NULL)
-EXECUTE FUNCTION adicionarItem();
+    AFTER UPDATE OF id_venda
+    ON Item FOR EACH ROW
+    WHEN (NEW.id_venda IS NULL AND OLD.id_venda IS NOT NULL)
+    EXECUTE FUNCTION adicionarItem();
 
 CREATE TRIGGER atualizarQuantidadeParaRemover
-AFTER UPDATE OF id_venda ON Item
-FOR EACH ROW
-WHEN (NEW.id_venda IS NOT NULL AND OLD.id_venda IS NULL)
-EXECUTE FUNCTION removerItem();
+    AFTER UPDATE OF id_venda
+    ON Item FOR EACH ROW
+    WHEN (NEW.id_venda IS NOT NULL AND OLD.id_venda IS NULL)
+    EXECUTE FUNCTION removerItem();
